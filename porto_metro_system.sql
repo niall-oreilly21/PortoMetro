314a91160aedd89f5b3d302b1d78d4936ef5178d
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-03-31 01:30:56 +0100

    :sparkles: Added journey routes table to the database.
    The journey routes will store all routes for a passenger.

--- porto_metro_system.sql
+++ porto_metro_system.sql
@@ -10,7 +10,7 @@ CREATE DATABASE IF NOT EXISTS porto_metro_system DEFAULT CHARACTER SET utf8 COLL
 USE porto_metro_system;
 
 /*DROPS any of the tables that may already exists*/
-DROP TABLE IF EXISTS station_facilities, facilities, stations_in_schedules, schedules, trains, lines, stations, cards, card_types, users, zones;
+DROP TABLE IF EXISTS station_facilities, facilities, stations_in_schedules, schedules, trains, station_lines, metro_lines, stations_in_journey_routes, journey_routes, stations, cards, users, card_types, zones;
 
 
 /*CREATE zones table*/
@@ -69,25 +69,61 @@ CREATE TABLE stations
     FOREIGN KEY (zone_id) REFERENCES zones(zone_id)
 );
 
+/*CREATE journey_routes table*/
+CREATE TABLE journey_routes 
+(
+    journey_route_id INT(3) NOT NULL,
+    user_id INT NOT NULL,
+    start_station_id VARCHAR(5) DEFAULT NULL,
+    end_station_id VARCHAR(5) DEFAULT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    fare DECIMAL(5,2) NOT NULL,
+    journey_route_description VARCHAR(255),
+    PRIMARY KEY (journey_route_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (start_station_id) REFERENCES stations(station_id),
+    FOREIGN KEY (end_station_id) REFERENCES stations(station_id)
+);
+
+/*CREATE stations_in_journey_routes table*/
+CREATE TABLE stations_in_journey_routes 
+(
+    journey_route_id INT(3) NOT NULL,
+    station_id VARCHAR(5) NOT NULL,
+    PRIMARY KEY (journey_route_id, station_id),
+    FOREIGN KEY (journey_route_id) REFERENCES journey_routes(journey_route_id),
+    FOREIGN KEY (station_id) REFERENCES stations(station_id)
+); 
+
 /*CREATE lines table*/
-CREATE TABLE lines
+CREATE TABLE metro_lines
 (
     line_id VARCHAR(1) NOT NULL,
     line_name VARCHAR(255),
     PRIMARY KEY (line_id)
 );
 
--- /*CREATE trains table*/
--- CREATE TABLE trains
--- (
---     train_id VARCHAR(4) NOT NULL,
---     line_id INT,
---     train_model VARCHAR(255),
---     carriages INT(2),
---     capacity INT(3),
---     PRIMARY KEY (train_id),
---     FOREIGN KEY (line_id) REFERENCES metro_lines(line_id)
--- );
+CREATE TABLE station_lines 
+(
+  station_id VARCHAR(5) NOT NULL,
+  line_id VARCHAR(1) NOT NULL,
+  PRIMARY KEY (station_id, line_id),
+  FOREIGN KEY (station_id) REFERENCES stations(station_id),
+  FOREIGN KEY (line_id) REFERENCES metro_lines(line_id)
+);
+
+/*CREATE trains table*/
+CREATE TABLE trains
+(
+    train_id VARCHAR(4) NOT NULL,
+    line_id VARCHAR(1),
+    train_model VARCHAR(255),
+    carriages INT(2),
+    capacity INT(3),
+    PRIMARY KEY (train_id),
+    FOREIGN KEY (line_id) REFERENCES metro_lines(line_id)
+);
 
 
 /*CREATE schedules table*/
@@ -96,8 +132,8 @@ CREATE TABLE schedules
     schedule_id VARCHAR(7) NOT NULL,
     train_id VARCHAR(4),
     schedule_description VARCHAR(255),
-    PRIMARY KEY (schedule_id)
-    -- FOREIGN KEY (train_id) REFERENCES trains(train_id)
+    PRIMARY KEY (schedule_id),
+    FOREIGN KEY (train_id) REFERENCES trains(train_id)
 );
 
 
