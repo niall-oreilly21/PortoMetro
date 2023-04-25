854b2e5fc361537da8608a2d829c8c09da593600
Luana Kimley <d00234604@student.dkit.ie>
2023-04-26 00:35:45 +0100

    :sparkles: Connect to line DAO to get line and route information in schedule page

--- src/main/java/gui/ScheduleController.java
+++ src/main/java/gui/ScheduleController.java
@@ -1,9 +1,16 @@
 package gui;
 
+import com.metroporto.dao.linedao.LineDaoInterface;
+import com.metroporto.dao.linedao.MySqlLineDao;
+import com.metroporto.enums.TimeTableType;
+import com.metroporto.exceptions.DaoException;
+import com.metroporto.metro.Route;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -25,6 +32,8 @@ import java.util.Objects;
 
 public class ScheduleController extends Controller
 {
+    LineDaoInterface lineDao;
+
     @FXML
     private HBox linesBox;
 
@@ -35,13 +44,22 @@ public class ScheduleController extends Controller
     private Line stationsLine;
 
     @FXML
-    private List<String> linesId = List.of("A", "B", "C", "D", "E", "F");
+    private ComboBox<String> daysComboBox;
+
+    @FXML
+    private Label endStationLabel;
+
+    private List<com.metroporto.metro.Line> lines;
 
     private ImageView selectedImageView;
 
     private List<String> currentLineStationsId;
 
     private List<String> currentLineStationsName;
+
+    private com.metroporto.metro.Line currentLine;
+
+    private Route currentRoute;
 
     private final Paint[] aColours = {Color.web("#0086de"), Color.web("#63beea")};
 
@@ -64,6 +82,20 @@ public class ScheduleController extends Controller
     private final double lineStartX = 50;
     private double lineEndX;
 
+    public ScheduleController()
+    {
+        lineDao = new MySqlLineDao();
+
+        try
+        {
+            lines = lineDao.findAll();
+        } catch (DaoException de)
+        {
+            System.out.println(de.getMessage());
+        }
+
+    }
+
     public void initialize()
     {
         Image logoImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/logo.png")));
@@ -75,16 +107,19 @@ public class ScheduleController extends Controller
         Image cardImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/card.png")));
         card.setImage(cardImage);
 
-        for (String lineId : linesId)
+        for (com.metroporto.metro.Line line : lines)
         {
             ImageView lineImageView = new ImageView();
             lineImageView.setFitWidth(70);
             lineImageView.setFitHeight(70);
             Image lineImage = new Image(
                     Objects.requireNonNull(
-                            getClass().getResourceAsStream("/img/lines/" + lineId + "_line.png")));
+                            getClass().getResourceAsStream("/img/lines/" + line.getLineId() + "_line.png")));
             lineImageView.setImage(lineImage);
             lineImageView.setOpacity(unselectedOpacity);
+
+            Tooltip tooltip = new Tooltip(line.getLineName());
+            Tooltip.install(lineImageView, tooltip);
 
             // Add mouse click event handler to each image view
             lineImageView.setOnMouseClicked(event ->
@@ -101,7 +136,10 @@ public class ScheduleController extends Controller
                 // Set the clicked ImageView as the selectedImageView
                 selectedImageView = lineImageView;
 
-                switch (lineId)
+                currentRoute = line.getRoutes().get(0);
+                endStationLabel.setText(currentRoute.getEndStation().getStationName());
+
+                switch (line.getLineId())
                 {
                     case "A":
                         currentLineStationsId = List.of("HSJ", "IPO", "PLU",
@@ -111,31 +149,37 @@ public class ScheduleController extends Controller
                                 "SGS", "CBT", "MRQ", "FGM", "TDD", "ALD",
                                 "SBT", "JDM", "GLT", "CMG", "JDD",
                                 "DJII", "STO", "LAL", "AKA");
+                        setCurrentLine("A");
                         selectedColours = aColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
                     case "B":
                         currentLineStationsId = List.of("B", "BB", "BBB", "BBBB", "BBBBB");
+                        setCurrentLine("B");
                         selectedColours = bColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
                     case "C":
                         currentLineStationsId = List.of("C", "CC", "CCC", "CCCC", "CCCCC");
+                        setCurrentLine("C");
                         selectedColours = cColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
                     case "D":
                         currentLineStationsId = List.of("D", "DD", "DDD", "DDDD", "DDDDD");
+                        setCurrentLine("D");
                         selectedColours = dColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
                     case "E":
                         currentLineStationsId = List.of("E", "EE", "EEE", "EEEE", "EEEEE");
+                        setCurrentLine("E");
                         selectedColours = eColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
                     case "F":
                         currentLineStationsId = List.of("F", "FF", "FFF", "FFFF", "FFFFF");
+                        setCurrentLine("F");
                         selectedColours = fColours;
                         drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
                         break;
@@ -147,7 +191,35 @@ public class ScheduleController extends Controller
             linesBox.getChildren().add(lineImageView);
         }
 
+        try
+        {
+            initialiseInitialLine();
+        }
+        catch (DaoException de)
+        {
+            System.out.println(de.getMessage());
+        }
+
+        stationsPane.widthProperty().addListener((obs, oldVal, newVal) ->
+        {
+            stationsLine.setStartX(lineStartX);
+            stationsLine.setEndX(lineEndX);
+        });
+
+        lineEndX = stationsPane.getPrefWidth() - 50;
+
+        drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
+
+        List<String> days = List.of(TimeTableType.MONDAY_TO_FRIDAY.getLabel(),
+                TimeTableType.SATURDAY.getLabel(), TimeTableType.SUNDAY.getLabel());
+        daysComboBox.getItems().addAll(days);
+        daysComboBox.getSelectionModel().selectFirst();
+    }
+
+    public void initialiseInitialLine() throws DaoException
+    {
         // Set the first image as the default selected image
+        setCurrentLine("A");
         selectedImageView = (ImageView) linesBox.getChildren().get(0);
         selectedImageView.setOpacity(selectedOpacity);
         currentLineStationsId = List.of("HSJ", "IPO", "PLU",
@@ -166,14 +238,8 @@ public class ScheduleController extends Controller
                 "Sao BenTo", "Jardim Do Morro", "GeneraL Torres", "CaMara Gaia", "Joao De Deus",
                 "D. Joao II", "SanTo Ovidio", "Lalalal", "Akakaka", "Lalalal", "Akakaka");
 
-        stationsPane.widthProperty().addListener((obs, oldVal, newVal) -> {
-            stationsLine.setStartX(lineStartX);
-            stationsLine.setEndX(lineEndX);
-        });
-
-        lineEndX = stationsPane.getPrefWidth() - 50;
-
-        drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
+        currentRoute = lineDao.findLineByLineId("A").getRoutes().get(0);
+        endStationLabel.setText(currentRoute.getEndStation().getStationName());
     }
 
     public void drawStationNodes(List<String> currentLineStationsId, List<String> currentLineStationsName,
@@ -187,10 +253,13 @@ public class ScheduleController extends Controller
         double textX = 32;
 
         // Check if stationsPane already has a Group child and remove if yes
-        if (stationsPane.getChildren().size() > 1 && stationsPane.getChildren().get(1) instanceof Group) {
+        if (stationsPane.getChildren().size() > 1 && stationsPane.getChildren().get(1) instanceof Group)
+        {
             List<Node> nodesToRemove = new ArrayList<>();
-            for (Node node : stationsPane.getChildren()) {
-                if (node instanceof Group) {
+            for (Node node : stationsPane.getChildren())
+            {
+                if (node instanceof Group)
+                {
                     nodesToRemove.add(node);
                 }
             }
@@ -200,18 +269,17 @@ public class ScheduleController extends Controller
         Group group = new Group();
         stationsPane.getChildren().add(group);
 
-        for (int i = 0; i < numStations; i++) {
+        for (int i = 0; i < numStations; i++)
+        {
             if (i == 0)
             {
                 drawStationNode(group, circleX, 10, currentLineStationsId.get(i),
                         currentLineStationsName.get(i), textX, colours[0], true);
-            }
-            else if (i == numStations - 1)
+            } else if (i == numStations - 1)
             {
                 drawStationNode(group, circleX += increment, 10, currentLineStationsId.get(i),
                         currentLineStationsName.get(i), textX += increment, colours[0], true);
-            }
-            else
+            } else
             {
                 drawStationNode(group, circleX += increment, 6, currentLineStationsId.get(i),
                         currentLineStationsName.get(i), textX += increment, colours[1], false);
@@ -250,6 +318,17 @@ public class ScheduleController extends Controller
         }
     }
 
+    private void setCurrentLine(String lineId)
+    {
+        try
+        {
+            currentLine = lineDao.findLineByLineId(lineId);
+        } catch (DaoException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public void redirectToHome(MouseEvent event) throws IOException
     {
         redirectToPage(event, Page.HOME);
@@ -283,5 +362,20 @@ public class ScheduleController extends Controller
             lineEndX = stationsPane.getPrefWidth() - 50;
             drawStationNodes(currentLineStationsId, currentLineStationsName, selectedColours);
         });
+    }
+
+    public void switchRoutes(ActionEvent event)
+    {
+        if (currentLine != null)
+        {
+            if (currentRoute.getRouteId() == currentLine.getRoutes().get(0).getRouteId())
+            {
+                currentRoute = currentLine.getRoutes().get(1);
+            } else
+            {
+                currentRoute = currentLine.getRoutes().get(0);
+            }
+            endStationLabel.setText(currentRoute.getEndStation().getStationName());
+        }
     }
 }
