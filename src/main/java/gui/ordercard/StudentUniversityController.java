552f7801ba7af4941ad9181b80300c2fa3bb38cd
Luana Kimley <d00234604@student.dkit.ie>
2023-05-02 23:57:53 +0100

    :sparkles: Develop student university page to show all universities from database

--- src/main/java/gui/ordercard/StudentUniversityController.java
+++ src/main/java/gui/ordercard/StudentUniversityController.java
@@ -1,8 +1,14 @@
 package gui.ordercard;
 
+import com.metroporto.dao.universitydao.MySqlUniversityDao;
+import com.metroporto.dao.universitydao.UniversityDaoInterface;
 import com.metroporto.enums.Folder;
+import com.metroporto.exceptions.DaoException;
+import com.metroporto.metro.University;
+import com.metroporto.users.Student;
 import gui.Controller;
 import com.metroporto.enums.Page;
+import gui.UniversityConverter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -11,16 +17,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentUniversityController extends Controller
 {
+    private UniversityDaoInterface universityDao;
+
     @FXML
     private Label universityLabel;
 
     @FXML
-    private ComboBox<String> universityComboBox;
+    private ComboBox<University> universityComboBox;
 
     @FXML
     private ToggleGroup cardToggleGroup;
@@ -31,42 +38,31 @@ public class StudentUniversityController extends Controller
     @FXML
     private RadioButton noCardRadioButton;
 
+    List<University> universities;
+
+    public StudentUniversityController()
+    {
+        user = app.getUser();
+
+        universityDao = new MySqlUniversityDao();
+
+        try
+        {
+            universities = universityDao.findAll();
+        } catch (DaoException de)
+        {
+            de.printStackTrace();
+        }
+    }
+
     public void initialize()
     {
         initialiseLogo();
         initialiseMetroImage("metro_2");
 
-        // TODO: this should be an ArrayList of universities fetched from the database
-        List<String> universities = new ArrayList<>();
-        universities.add("Option 1");
-        universities.add("Option 2");
-        universities.add("Option 3");
-
+        universityComboBox.setPromptText("Choose university...");
         universityComboBox.getItems().addAll(universities);
-
-        universityComboBox.getItems().add(0, "Choose university...");
-        universityComboBox.getSelectionModel().selectFirst();
-
-        // Make first item (Choose university...) grey & disabled
-        universityComboBox.setCellFactory(param -> new ListCell<>()
-        {
-            @Override
-            protected void updateItem(String item, boolean empty)
-            {
-                super.updateItem(item, empty);
-
-                if (empty || item == null)
-                {
-                    setText(null);
-                    setDisable(false);
-                } else
-                {
-                    setText(item);
-                    setDisable(getIndex() == 0);
-                    setStyle(getIndex() == 0 ? "-fx-text-fill: gray;" : "");
-                }
-            }
-        });
+        universityComboBox.setConverter(new UniversityConverter());
 
         cardToggleGroup = new ToggleGroup();
         yesCardRadioButton.setToggleGroup(cardToggleGroup);
@@ -76,38 +72,43 @@ public class StudentUniversityController extends Controller
     public void setScene(Scene scene)
     {
         scene.heightProperty().addListener((observable, oldValue, newValue) ->
-            metro1.fitHeightProperty().setValue(newValue)
+                metro1.fitHeightProperty().setValue(newValue)
         );
     }
 
     @FXML
     private void submitForm(ActionEvent event) throws IOException
     {
-        String university = universityComboBox.getValue();
         String asterisk = "*";
         String errorColour = "#de2a1d";
 
         Text redAsterisk = new Text(asterisk);
         redAsterisk.setFill(Color.web(errorColour));
 
-        if (!university.equals(universityComboBox.getItems().get(0)))
+        if (universityComboBox.getSelectionModel().getSelectedItem() != null)
         {
+            University university = universityComboBox.getValue();
+
             universityLabel.setGraphic(null);
             errorText.setText("");
 
-            // TODO: add the student university data to database
-            System.out.println(university);
+            try
+            {
+                ((Student) user).setUniversity(university);
+                universityDao.insertUniversityForStudent(user);
+            } catch (DaoException de)
+            {
+                de.printStackTrace();
+            }
 
             if (cardToggleGroup.getSelectedToggle() == yesCardRadioButton)
             {
                 redirectToPage(event, Folder.ORDER_CARD, Page.CARD_ZONE);
-            }
-            else
+            } else
             {
                 redirectToPage(event, Folder.HOME, Page.HOME);
             }
-        }
-        else
+        } else
         {
             errorText.setText(asterisk + " Please choose an option");
             universityLabel.setGraphic(redAsterisk);
